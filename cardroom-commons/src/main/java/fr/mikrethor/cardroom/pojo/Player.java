9b148ada3c71b281d9fbd690f0a3b521480568b6
Xavier BOUCLET <mikrethor@gmail.com>
2017-01-19 20:26:21 -0500

    Adding equals method to Player

--- cardroom-commons/src/main/java/fr/mikrethor/cardroom/pojo/Player.java
+++ cardroom-commons/src/main/java/fr/mikrethor/cardroom/pojo/Player.java
@@ -22,10 +22,53 @@ public class Player {
 	private Cardroom cardroom;
 	@NonNull
 	private String name;
-	
-    private Set<Account> accountDTOes = new HashSet<Account>(0);
-    private Integer seat;
-    private Boolean on;
-    private Double stack;
-    private Card[] cards;
+
+	private Set<Account> accountDTOes = new HashSet<Account>(0);
+	private Integer seat;
+	private Boolean on;
+	private Double stack;
+	private Card[] cards;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cardroom == null) ? 0 : cardroom.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Player other = (Player) obj;
+		if (cardroom == null) {
+			if (other.cardroom != null) {
+				return false;
+			}
+		} else {
+			if (!cardroom.equals(other.cardroom)) {
+				return false;
+			}
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else {
+			if (!name.equals(other.name)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 }
