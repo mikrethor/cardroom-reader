3b6e7ef5dd8cd7877969bf3fd98842c90d46375f
Xavier BOUCLET <mikrethor@gmail.com>
2017-02-14 23:19:20 -0500

    Using spring instance to instantiate dozer mapping

--- cardroom-ws/src/main/java/fr/mikrethor/cardroom/ws/config/Application.java
+++ cardroom-ws/src/main/java/fr/mikrethor/cardroom/ws/config/Application.java
@@ -1,5 +1,6 @@
 package fr.mikrethor.cardroom.ws.config;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -7,6 +8,8 @@ import java.util.Properties;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp.BasicDataSource;
+import org.dozer.DozerBeanMapper;
+import org.dozer.spring.DozerBeanMapperFactoryBean;
 import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +18,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -127,4 +132,17 @@ public class Application {
 		};
 	}
 
+	@Bean
+	public DozerBeanMapperFactoryBean mapperFactory() throws IOException {
+		DozerBeanMapperFactoryBean mapper = new DozerBeanMapperFactoryBean();
+		Resource[] resources = new PathMatchingResourcePatternResolver().getResources("classpath*:mappingDozer.xml");
+		mapper.setMappingFiles(resources);
+		return mapper;
+	}
+
+	@Bean
+	public DozerBeanMapper mapper(DozerBeanMapperFactoryBean mapperFactory) throws Exception {
+		DozerBeanMapper mapper = (DozerBeanMapper) mapperFactory.getObject();
+		return mapper;
+	}
 }
--- cardroom-ws/src/test/java/fr/mikrethor/cardroom/ws/mapping/MappingDozerTest.java
+++ cardroom-ws/src/test/java/fr/mikrethor/cardroom/ws/mapping/MappingDozerTest.java
@@ -1,20 +1,31 @@
 package fr.mikrethor.cardroom.ws.mapping;
 
+import org.dozer.DozerBeanMapper;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import fr.mikrethor.cardroom.enums.Domain;
 import fr.mikrethor.cardroom.pojo.Cardroom;
+import fr.mikrethor.cardroom.ws.config.Application;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
 public class MappingDozerTest {
 	private static final Logger LOGGER = LoggerFactory.getLogger(MappingDozerTest.class);
 	long after;
 	long before;
 	Cardroom cardroom = null;
+
+	@Autowired
+	DozerBeanMapper mapper;
 
 	@Before
 	public void setUp() {
@@ -25,9 +36,10 @@ public class MappingDozerTest {
 
 	@Test
 	public void cardroomMappingTest() {
-		final MapperSingleton mapper = MapperSingleton.getInstance();
 		final fr.mikrethor.cardroom.ws.jpa.Cardroom site = mapper.map(cardroom,
 				fr.mikrethor.cardroom.ws.jpa.Cardroom.class);
+		Assert.assertNotNull(site.getName());
+		Assert.assertNotNull(site.getDomain());
 		Assert.assertEquals(site.getName(), cardroom.getName());
 		Assert.assertEquals(site.getDomain(), cardroom.getDomain());
 
